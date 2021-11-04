08d19e9499af4c13a1ad8e5a0243f1fd1e33f85e
DikerZarc <88402701+DikerZarc40@users.noreply.github.com>
2021-11-03 20:02:40 -0600

    Add files via upload

---
+++ Metadata.py
@@ -0,0 +1,65 @@
+
+from PIL.ExifTags import TAGS, GPSTAGS
+from PIL import Image
+import os
+
+def decode_gps_info(exif):
+    gpsinfo = {}
+    if 'GPSInfo' in exif:
+        Nsec = exif['GPSInfo'][2][2] 
+        Nmin = exif['GPSInfo'][2][1]
+        Ndeg = exif['GPSInfo'][2][0]
+        Wsec = exif['GPSInfo'][4][2]
+        Wmin = exif['GPSInfo'][4][1]
+        Wdeg = exif['GPSInfo'][4][0]
+        if exif['GPSInfo'][1] == 'N':
+            Nmult = 1
+        else:
+            Nmult = -1
+        if exif['GPSInfo'][1] == 'E':
+            Wmult = 1
+        else:
+            Wmult = -1
+        Lat = Nmult * (Ndeg + (Nmin + Nsec/60.0)/60.0)
+        Lng = Wmult * (Wdeg + (Wmin + Wsec/60.0)/60.0)
+        exif['GPSInfo'] = {"Lat" : Lat, "Lng" : Lng}
+        input()
+
+ 
+def get_exif_metadata(image_path):
+    ret = {}
+    image = Image.open(image_path)
+    if hasattr(image, '_getexif'):
+        exifinfo = image._getexif()
+        if exifinfo is not None:
+            for tag, value in exifinfo.items():
+                decoded = TAGS.get(tag, tag)
+                ret[decoded] = value
+    decode_gps_info(ret)
+    return ret
+    
+
+ruta = input("Ruta de imágenes: ")
+os.chdir(ruta)
+for root, dirs, files in os.walk(".", topdown=False):
+    for name in files:
+        print(os.path.join(root, name))
+        print ("[+] Metadata for file: %s " %(name))
+        with open('metadata para imagenes.txt', 'a') as wt:
+            wt.write("\n [+] Metadata for files: %s" %(name))
+            print("\n")
+        input()
+        try:
+            exifData = {}
+            exif = get_exif_metadata(name)
+            for metadata in exif:
+                print ("Metadata: %s - Value: %s " %(metadata, exif[metadata]))
+                with open('metadata para imagenes.txt', 'a') as wt:
+                    wt.write("\n Metadata: %s - Value: %s " %(metadata, exif[metadata]))
+            print ("\n")
+        except:
+            import sys, traceback
+            traceback.print_exc(file=sys.stdout)
+
+
+
